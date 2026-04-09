00ed6df26fe620a8d6352ce8fcd23f39faa7470d
kkef26 <kefalos.kosta@gmail.com>
2026-04-09 16:29:22 +0300

    fix: add S3 image remotePatterns to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "anadomisi-documents.s3.eu-west-1.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
